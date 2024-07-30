a3ef9c15cae62fb2b0258655e7f89b06492f3c5d
Raikiri45 <149266085+Raikiri45@users.noreply.github.com>
2024-07-30 14:44:33 +0100

    created all the button reference variables

--- javascript.js
+++ javascript.js
@@ -4,7 +4,27 @@ let operator = '';
 let num2 = '';
 
 const display = document.querySelector('#display');
+
 const buttonOne = document.querySelector('#one');
+const buttonTwo = document.querySelector('#two');
+const buttonThree = document.querySelector('#three');
+const buttonFour = document.querySelector('#four');
+const buttonFive = document.querySelector('#five');
+const buttonSix = document.querySelector('#six');
+const buttonSeven = document.querySelector('#seven');
+const buttonEight = document.querySelector('#eight');
+const buttonNine = document.querySelector('#nine');
+
+const addButton = document.querySelector('#add');
+const subtractButton = document.querySelector('#subtract');
+const divideButton = document.querySelector('#divide');
+const multiplyButton = document.querySelector('#multiply');
+const remainderButton = document.querySelector('#remainder');
+
+const allClearButton = document.querySelector('#allClear');
+const decimalButton = document.querySelector('#decimalPlace');
+const plusMinus = document.querySelector('#plusMinus');
+const equalsButton = document.querySelector('#equals');
 
 function inputOne(){
     if (num1.length < 10 && num1.includes('.'))
